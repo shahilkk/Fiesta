42b7355d20c316e0061929f14e5ab3ce31e458ac
shahilkhan123 <shahilkhanarimbra@gmail.com>
2022-04-21 16:13:31 +0530

    database created

--- adminapp/models.py
+++ adminapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Customer(models.Model):
+    client_name = models.CharField(max_length=30) 
+    gst_number = models.CharField(max_length=30) 
+    client_id = models.CharField(max_length=30) 
+    phone = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    district = models.CharField(max_length=30)
+    district = models.CharField(max_length=30)
+    zipcode = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
